f419228ede6b88e82ce26f1911234ddc36174b84
Urbenson Laurent <ulaurent12@gmail.com>
2018-09-29 21:33:06 +0000

    LED toggle function

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,17 +1,29 @@
 /*First Program to toggle LED L*/
 
+// Global variables
+int leadPin = 13;
+
+void primeLight(){
+  // put your main code here, to run repeatedly:
+  digitalWrite (leadPin, HIGH); // High Meaning On
+  delay(5000); // Delay between on to off, 2 seconds
+  digitalWrite (leadPin, LOW);
+  delay(10000);
+}
+
+void secLight(){
+  digitalWrite (leadPin, HIGH);
+  delay(3000);
+  digitalWrite (leadPin, LOW); // Low meaning off
+  delay(2000); // Delay between on to off, 2 seconds
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT); // Which pin, what kind of output
+  pinMode(leadPin, OUTPUT); // Which pin, what kind of output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite (13, HIGH); // High Meaning On
-
-  delay(5000); // Delay between on to off, 2 seconds 
-  
-  digitalWrite (13, LOW); // Low meaning off
-
-  delay(500); // Delay between on to off, 2 seconds
+   primeLight();
+   secLight();
 }
